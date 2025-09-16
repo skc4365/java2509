19363d91b6cb2d4017c357820cae6fade4074fe7
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-09-16 10:10:52 +0900

    add 추상클래스

--- class-proj/src/com/skc/Main.java
+++ class-proj/src/com/skc/Main.java
@@ -1,5 +1,6 @@
 package com.skc;
 
+import com.skc.abst.AbstractMain;
 import com.skc.ext.ExtendsMain;
 import com.skc.poly.PolymorphismMain;
 
@@ -11,7 +12,10 @@ public class Main {
 //		ExtendsMain extendsMain = new ExtendsMain();
 
 //		다형성 polymorphism
-		PolymorphismMain polymorphismMain = new PolymorphismMain();
+//		PolymorphismMain polymorphismMain = new PolymorphismMain();
+
+//		추상클래스 abstract
+		AbstractMain abstractMain = new AbstractMain();
 	}
 
 }
